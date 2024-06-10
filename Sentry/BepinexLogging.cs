5f17b5bdb868dbc5893dc86cb54a369c66e954da
ujkkju117 <ujkkju117@outlook.com>
2024-06-10 17:21:04 +0930

    update logging to do a lookup on log level to skip creating sentry instance if not sending a log.

--- Sentry/BepinexLogging.cs
+++ Sentry/BepinexLogging.cs
@@ -1,43 +1,85 @@
-﻿using BepInEx.Logging;
+﻿using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Logging;
+using PluginUtilities;
 using Sentry;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine.SceneManagement;
 
 namespace Logging
 {
-    public abstract class BepinexLogging
+    [BepInPlugin(Guid, "Config Manager", Version)]
+    [BepInDependency(SetInjectionFlag.Guid)]
+    public sealed class BepinexLogging : BaseUnityPlugin
     {
-        protected ManualLogSource Logger { get; }
- 
-        private SentryOptions _sentryOptions;
-        private EventHandler<LogEventArgs> logEvent;
+        /// <summary>
+        /// Plugin Attributes
+        /// </summary>
+        public const string Guid = "com.hf.hollofox.sentry";
+        public const string Name = "Bepinex To Sentry Logging";
+        public const string Version = "1.0.1.0";
+        private const BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.NonPublic;
 
-        protected SentryOptions sentryOptions
+        /// <summary>
+        /// 
+        /// </summary>
+        private bool addLogForwarding = true;
+        private readonly Dictionary<string, string> ApiKeyLookup = new Dictionary<string, string>() {
+            
+        };
+
+        public BepinexLogging()
         {
-            get => _sentryOptions; 
-            set {
-                if (logEvent != null)
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (addLogForwarding)
+            {
+                addLogForwarding = false;
+
+                Logger.LogInfo("Initializing Sentry Log Forwarding");
+
+                Chainloader.Plugins
+                .Concat(FindObjectsOfType(typeof(BaseUnityPlugin)).Cast<BaseUnityPlugin>()).Distinct()
+                .Select(p => (plugin: p,metadata: MetadataHelper.GetMetadata(p))).ToList()
+                .ForEach(p =>
                 {
-                    Logger.LogEvent -= logEvent;
-                    logEvent = null;
-                }
-                if (value != null)
-                {
-                    logEvent = GenerateSentryLogFowarding(value, GetType().Assembly.GetName().Version.ToString());
-                    Logger.LogEvent += logEvent;
-                }
-                _sentryOptions = value; 
+                    var plugin = p.plugin;
+                    var metadata = p.metadata;
+                    if (ApiKeyLookup.TryGetValue(p.metadata.Name, out string apiKey))
+                    {
+                        Logger.LogInfo($"Fetching logSource for {metadata.Name}");
+                        ManualLogSource manualLogSource = (ManualLogSource)typeof(BaseUnityPlugin).GetProperty("Logger", bindFlags).GetValue(plugin); // use sys reflect to fetch
+
+                        if (manualLogSource != null)
+                        {
+                            Logger.LogInfo($"Found {metadata.Name} manual log source");
+                            SentryOptions sentryOptions = new SentryOptions
+                            {
+                                Dsn = apiKey,
+                                Debug = true,
+                                TracesSampleRate = 1,
+                                IsGlobalModeEnabled = false,
+                                AttachStacktrace = true
+                            };
+                            Logger.LogInfo($"{metadata.Name} sentry options created");
+
+                            manualLogSource.LogEvent += GenerateSentryLogFowarding(sentryOptions, metadata.Version.ToString());
+                            Logger.LogInfo($"Added Sentry Log Forwarding for {metadata.Name}");
+                        }
+                    }
+                });
+
+                Logger.LogInfo("Sentry Log Forwarding Initialized");
             }
         }
 
-        protected BepinexLogging()
-        {
-            string LoggerID = GetType().AssemblyQualifiedName;
-            Logger = BepInEx.Logging.Logger.CreateLogSource(LoggerID);
-        }
-
-        /// <inheritdoc cref="GenerateSentryLogFowarding(SentryOptions, string)"/>
-        private EventHandler<LogEventArgs> GenerateSentryLogFowarding(string pluginVersion)
-            => GenerateSentryLogFowarding(sentryOptions, pluginVersion);
+        private static readonly HashSet<LogLevel> accepted = new HashSet<LogLevel> { LogLevel.Fatal, LogLevel.Error, LogLevel.Warning };
 
         /// <summary>
         /// Generates a log forwarding function for Sentry.
@@ -46,6 +88,8 @@ namespace Logging
         {
             void output(object sender, LogEventArgs e)
             {
+                if (accepted.Contains(e.Level))
+                {
                 void _scope(Scope scope)
                 {
                     scope.User = new User
@@ -70,6 +114,7 @@ namespace Logging
                             break;
                     }
                 }
+            }
             }
 
             return output;
